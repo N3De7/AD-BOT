9bc853f8586342f4310348afeab2d3e647c850d6
N3De7 <shahinvnx07@gmail.com>
2026-07-24 00:36:02 +0330

    adbot.py

--- adbot.py
+++ adbot.py
@@ -2713,7 +2713,7 @@ async def main():
     from http.server import BaseHTTPRequestHandler, HTTPServer
     
     logger.info("تلاش برای بارگذاری متغیرهای محیطی...")
-    room_id = os.getenv("ROOM_ID", "69a02f48c98665697d5bfbb0")
+    room_id = os.getenv("ROOM_ID", "")
     api_token = os.getenv("API_TOKEN", "bec20fb13d70caef9cb1a113aed43fa8bcaf1d9b8277adbe9908ee278c8a7df3")
     
     if not room_id or not api_token:
